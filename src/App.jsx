410fd8879c6921cefbbd44f338e721128d29ff12
Viktor.A <viktor.antonyshyn@gmail.com>
2023-07-07 13:08:51 +0200

    Added Head.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Head from './Components/Head'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <p className="read-the-docs">
-        HEAD
-      </p>
+      < Head/>
     </>
   )
 }
