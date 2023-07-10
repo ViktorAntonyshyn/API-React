dd1a804a4f044242a7a3e8b6f114d60e616ec40d
Viktor.A <viktor.antonyshyn@gmail.com>
2023-07-11 01:06:05 +0200

    Fixed push-up

--- src/Components/CharacterCardsHPpu.jsx
+++ src/Components/CharacterCardsHPpu.jsx
@@ -14,13 +14,11 @@ const CharacterCardsHPpu = () => {
 
   const handleCardClick = (name, gender, species, house) => {
     setSelectedCharacter(name); // Set selectedCharacter por personaje
-    alert(`Character: ${name}\nGender: ${gender}\nSpecies: ${species}\nHouse: ${house}`);
+    //alert(`Character: ${name}\nGender: ${gender}\nSpecies: ${species}\nHouse: ${house}`);
     
     setTimeout(() => {
     setSelectedCharacter(null); 
-    }, 4000); //Tiempo a volver push-up
-  
-  
+    }, 3000); //Tiempo a volver push-up
   };
 
   return (
@@ -33,15 +31,9 @@ const CharacterCardsHPpu = () => {
             className={`card ${selectedCharacter === character.name ? "selected" : ""}`} 
             key={character.name}
             onClick={() => handleCardClick(character.name)} 
-            data-tooltip={`Characte: ${character.name}\nGender: ${character.gender}\nSpecies: ${character.species}\nHouse: ${character.house}`}>
-            <p>
-              <span>Gender:</span> {character.gender}
-            </p>
-            <img
-              className="character-image"
-              src={character.image}
-              alt={character.name}
-            />
+            data-tooltip={`Nombre: ${character.name}\nGénero: ${character.gender}\nEspecies: ${character.species}\nEscuela: ${character.house}`}>
+            <p><span>Gender:&nbsp;</span>{character.gender}</p>
+            <img className="character-image" src={character.image} alt={character.name} />
             <p className="character-info">{character.name}</p>
           </div>
         ))}
